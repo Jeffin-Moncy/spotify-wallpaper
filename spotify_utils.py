15088b7c45836411f4b880f23db5c6f141a74890
Jeffin Moncy <jeffinmoncy25@gmail.com>
2025-05-26 15:44:38 +0530

    Name Summarize Update

--- spotify_utils.py
+++ spotify_utils.py
@@ -16,6 +16,20 @@ client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
+def format_title(raw_title):
+    for symbol in ["(", "[", "{"]:
+        if symbol in raw_title:
+            return raw_title.split(symbol)[0].strip()
+    return raw_title.strip()
+
+def format_artists(artist_list):
+    if len(artist_list) <= 2:
+        return ", ".join(artist_list)
+
+    joined = ", ".join(artist_list[:3])
+    return joined if len(joined) <= 40 else joined[:32].rstrip(", ") + "..."
+
+
 def extract_spotify_id(url: str):
     pattern = r"spotify\.com/(track|album)/([A-Za-z0-9]+)"
     match = re.search(pattern, url)
@@ -34,8 +48,12 @@ def get_spotify_metadata(spotify_url: str):
         else:
             raise ValueError("Unsupported Spotify content type.")
 
-        title     = data["name"]
-        artist    = ", ".join(a["name"] for a in data["artists"])
+        raw_title   = data["name"]
+        raw_artists = [a["name"] for a in data["artists"]]
+
+        title  = format_title(raw_title)
+        artist = format_artists(raw_artists)
+
         cover_url = data["album"]["images"][0]["url"] if kind == "track" else data["images"][0]["url"]
         track_id  = spotify_url.split("/")[-1].split("?")[0]
 
